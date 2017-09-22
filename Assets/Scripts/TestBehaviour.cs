a6efa9057bec73d6ae1075360595fa820115103f
Matheus Lessa <matheuslessarodrigues@gmail.com>
2017-09-22 00:51:31 -0300

    stuff

---
+++ Assets/Scripts/TestBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.AttributeUsage(System.AttributeTargets.Field)]
+public class PotionSelectorAttribute : PropertyAttribute
+{
+}
+
+public class TestBehaviour : MonoBehaviour
+{
+	[PotionSelector]
+	public string potionName;
+
+	public void Start()
+	{
+		// PotionDB.Instance pra acessar o "banco de dados" de pocoes
+		// Imprimir o quanto aquela potion recupera
+	}
+}
